d115e64067fb5b7ff2e8e0c2da1cd08ea0d2c4f3
Paul-Hadfield <732223+Paul-Hadfield@users.noreply.github.com>
2025-11-28 21:08:42 +0000

    Converted to typescript

---
+++ src/grid.tsx
@@ -0,0 +1,42 @@
+import Cell from './cell';
+import type { Cell as CellType } from './gridsetup';
+
+type Dimensions = {
+  width: number;
+  height: number;
+  rows: number;
+  cols: number;
+};
+
+type Props = {
+  dimensions: Dimensions;
+  gameGrid: CellType[];
+};
+
+const Grid = ({ dimensions, gameGrid }: Props) => {
+  const cellSize = Math.floor(
+    Math.min(
+      ...[
+        ((dimensions.width - 100) * 0.8) / dimensions.cols,
+        (dimensions.height * 0.8) / dimensions.rows,
+        200,
+      ],
+    ),
+  );
+
+  const styles = {
+    width: `${dimensions.cols * cellSize}px`,
+    gridTemplateColumns: `repeat(${dimensions.cols}, ${cellSize}px)`,
+    gridTemplateRows: `repeat(${dimensions.rows}, ${cellSize}px)`,
+  };
+
+  return (
+    <div className="grid" style={styles}>
+      {gameGrid.map((cell, index) => (
+        <Cell key={index} cell={cell} />
+      ))}
+    </div>
+  );
+};
+
+export default Grid;
